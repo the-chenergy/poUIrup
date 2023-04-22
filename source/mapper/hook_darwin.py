73d046193ff900e7124e59c751986c4cf44c2591
Qianlang Chen <qianlangchen@gmail.com>
2023-04-21 23:21:17 -0700

    Show UI event type names in the output
    Event types are encoded as an int in the callback. Now the demo shows
    the corresponding constant name in code in addition to the int.
    
    - An example of a constant name is `NSEventTypeLeftMouseDown`. Access
      this constant simply with `Quartz.NSEventTypeLeftMouseDown`.
    
    Feel free to use the created playground.ipynb notebook for
    experimentation.

--- source/mapper/hook_darwin.py
+++ source/mapper/hook_darwin.py
@@ -10,9 +10,44 @@ class Context(typing.NamedTuple):
 
 def create() -> Context:
     def handle_native_event(proxy, event_type, event, user_data):
-        print(
-            f'{proxy=} {type(proxy)=} {event_type=} {type(event_type)=} {event=} {type(event)=} {user_data=} {type(user_data)=}'
-        )
+        EVENT_TYPE_CONSTANT_NAMES = {
+            1: 'NSEventTypeLeftMouseDown',
+            2: 'NSEventTypeLeftMouseUp',
+            3: 'NSEventTypeRightMouseDown',
+            4: 'NSEventTypeRightMouseUp',
+            5: 'NSEventTypeMouseMoved',
+            6: 'NSEventTypeLeftMouseDragged',
+            7: 'NSEventTypeRightMouseDragged',
+            8: 'NSEventTypeMouseEntered',
+            9: 'NSEventTypeMouseExited',
+            10: 'NSEventTypeKeyDown',
+            11: 'NSEventTypeKeyUp',
+            12: 'NSEventTypeFlagsChanged',
+            13: 'NSEventTypeAppKitDefined',
+            14: 'NSEventTypeSystemDefined',
+            15: 'NSEventTypeApplicationDefined',
+            16: 'NSEventTypePeriodic',
+            17: 'NSEventTypeCursorUpdate',
+            18: 'NSEventTypeRotate',
+            19: 'NSEventTypeBeginGesture',
+            20: 'NSEventTypeEndGesture',
+            22: 'NSEventTypeScrollWheel',
+            23: 'NSEventTypeTabletPoint',
+            24: 'NSEventTypeTabletProximity',
+            25: 'NSEventTypeOtherMouseDown',
+            26: 'NSEventTypeOtherMouseUp',
+            27: 'NSEventTypeOtherMouseDragged',
+            29: 'NSEventTypeGesture',
+            30: 'NSEventTypeMagnify',
+            31: 'NSEventTypeSwipe',
+            32: 'NSEventTypeSmartMagnify',
+            33: 'NSEventTypeQuickLook',
+            34: 'NSEventTypePressure',
+            37: 'NSEventTypeDirectTouch',
+            38: 'NSEventTypeChangeMode',
+        }
+        print(EVENT_TYPE_CONSTANT_NAMES.get(event_type, '(unknown)'),
+              event_type)
         return event
 
     event_tap = Quartz.CGEventTapCreate(Quartz.kCGAnnotatedSessionEventTap,
